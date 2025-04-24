992a46872f8f0af134db23a37cb86502f30a8947
OverflowCat <overflowcat@gmail.com>
2025-04-25 00:53:54 +0800

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+# `shiki-git-diff`
+
+## Install
+
+```sh
+pnpm add shiki-git-diff
+```
+
+## Configure
+
+```js
+import shikiGitDiff from "shiki-git-diff";
+    // ...
+    transformers: [
+        shikiGitDiff({
+            // default config
+            classLineAdd: "add",
+            classLineRemove: "remove",
+            classActivePre: "diff",
+            condition: (meta: string) => meta.endsWith("diff"),
+        }),
+    ],
+    // ...
+```
+
+## Display
+
+```less
+code>[data-line] {
+    &.diff.add {
+        background-color: rgba(67, 218, 64, 0.3);
+    }
+    &.diff.remove {
+        background-color: rgba(229, 38, 38, 0.3);
+    }
+}
+```
---
+++ index.ts
@@ -0,0 +1,46 @@
+import { ShikiTransformer } from "shiki";
+
+type shikiGitDiffOptions = {
+    classLineAdd?: string;
+    classLineRemove?: string;
+    classActivePre?: string;
+    condition?: (meta: string) => boolean;
+};
+
+function shikiGitDiff(
+    options: shikiGitDiffOptions = {}
+): ShikiTransformer {
+    const {
+        classLineAdd = "add",
+        classLineRemove = "remove",
+        classActivePre = "diff",
+        condition = (meta: string) => meta.endsWith("diff"),
+    } = options;
+    return {
+        name: "shiki-git-diff",
+        code: function (hast) {
+            // @ts-expect-error
+            if (!condition(this.options.meta.__raw)) return;
+            for (const line of hast.children) {
+                if (line.type !== "element") {
+                    continue;
+                }
+                const firstSpan = line?.children?.at(0);
+                let firstChar: string | undefined = undefined;
+                if (firstSpan?.type === "text") {
+                    firstChar = firstSpan.value[0];
+                } else if (firstSpan?.type === "element") {
+                    const firstText = firstSpan?.children?.at(0);
+                    if (firstText?.type === "text") {
+                        firstChar = firstText.value?.at(0);
+                    }
+                }
+                if (firstChar === "+") {
+                    this.addClassToHast(line, [classActivePre, classLineAdd]);
+                } else if (firstChar === "-") {
+                    this.addClassToHast(line, [classActivePre, classLineRemove]);
+                }
+            }
+        }
+    }
+}
